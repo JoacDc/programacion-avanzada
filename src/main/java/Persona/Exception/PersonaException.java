a218d4285dba9adac2a423de3b4a5a82564de229
JoacDc <delcantodiegojoaquin5@gmail.com>
2025-09-24 12:03:56 -0300

    subi mal cosas al main y las deje en develop

--- src/main/java/Persona/Exception/PersonaException.java
+++
@@ -1,7 +0,0 @@
-package Persona.Exception;
-
-public class PersonaException extends RuntimeException {
-    public PersonaException(String message) {
-        super(message);
-    }
-}
--- src/main/java/Persona/Exception/RepositorioException.java
+++
@@ -1,7 +0,0 @@
-package Persona.Exception;
-
-public class RepositorioException extends RuntimeException {
-    public RepositorioException(String message) {
-        super(message);
-    }
-}
